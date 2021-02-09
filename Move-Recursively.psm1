a7bd475e6ac1ac318315e9a438569ca925250e12
Alaknar <26569304+Alaknar@users.noreply.github.com>
2021-02-09 21:27:30 +0100

    Adding modules

---
+++ Move-Recursively.psm1
@@ -0,0 +1,17 @@
+﻿function Move-Recursively {
+
+$OrgPath = Read-host "Enter Source Path"
+$NewPath = Read-host "Enter Destination Folder"
+$FileType = Read-host "Provide the file type (e.g. '.mp3'). Include the fullstop"
+$files = get-childitem -recurse -file | where {$_.extension -match "$filetype"}
+
+Foreach($file In $files)
+    {
+        $Directory = ($file.fullname.split("\")[0..($file.fullname.split("\").count -2)] -join "\").replace("$OrgPath","$NewPath")
+        if((Test-path -path $Directory) -eq $False)
+        {
+            new-item $Directory -type directory
+        }
+        Move-Item -path $File.fullname -destination $directory
+    }
+}

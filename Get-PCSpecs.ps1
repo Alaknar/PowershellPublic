0d12ae76c5754709b18afa110264d2137ecaaf1d
Alaknar <26569304+Alaknar@users.noreply.github.com>
2022-05-31 00:37:41 +0200

    Formatting updates

--- Get-PCSpecs.ps1
+++ Get-PCSpecs.ps1
@@ -1,24 +1,24 @@
 function Get-PCSpecs {
-    $MoBo           = Get-CimInstance -ClassName Win32_BaseBoard
-    $CPU            = Get-CimInstance -ClassName Win32_Processor
-    $GPU            = Get-CimInstance -ClassName Win32_VideoController
-    $RAM            = Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum
-    $Disks          = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Select-Object -Property DeviceID,VolumeName,@{Name="TotalSize";Expression={([math]::round(($_.Size / 1GB))).ToString()+" GB"}},@{Name="FreeSpaceGB";Expression={([math]::round(($_.FreeSpace / 1GB))).ToString()+" GB"}}
-    $Monitors       = Get-CimInstance -Namespace root\wmi -ClassName wmimonitorid | ForEach-Object {
-				New-Object -TypeName psobject -Property @{
-        			Manufacturer = ($_.ManufacturerName -notmatch '^0$' | ForEach-Object {[char]$_}) -join ""
-        			Name = ($_.UserFriendlyName -notmatch '^0$' | ForEach-Object {[char]$_}) -join ""
-        			Serial = ($_.SerialNumberID -notmatch '^0$' | ForEach-Object {[char]$_}) -join ""
-   				}
+    $MoBo       = Get-CimInstance -ClassName Win32_BaseBoard
+    $CPU        = Get-CimInstance -ClassName Win32_Processor
+    $GPU        = Get-CimInstance -ClassName Win32_VideoController
+    $RAM        = Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum
+    $Disks      = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Select-Object -Property DeviceID,VolumeName,@{Name="TotalSize";Expression={([math]::round(($_.Size / 1GB))).ToString()+" GB"}},@{Name="FreeSpaceGB";Expression={([math]::round(($_.FreeSpace / 1GB))).ToString()+" GB"}}
+    $Monitors   = Get-CimInstance -Namespace root\wmi -ClassName wmimonitorid | ForEach-Object {
+                    New-Object -TypeName psobject -Property @{
+                        Manufacturer = ($_.ManufacturerName -notmatch '^0$' | ForEach-Object {[char]$_}) -join ""
+                        Name = ($_.UserFriendlyName -notmatch '^0$' | ForEach-Object {[char]$_}) -join ""
+                        Serial = ($_.SerialNumberID -notmatch '^0$' | ForEach-Object {[char]$_}) -join ""
+                    }
 			}
 
-    $computerdeets = [pscustomobject]@{
-        MoBo            = $MoBo.Manufacturer+" "+$MoBo.Product
-        CPU             = $CPU.DeviceID+" | "+$CPU.Name
-        GPU             = $GPU.Name+" | "+[math]::round($GPU.AdapterRAM / 1GB)+"GB | Driver version: "+$GPU.DriverVersion+" (published date: "+$GPU.DriverDate+")"
-        RAM             = ([math]::round(($RAM.Sum / 1GB))).ToString()+"GB in "+$RAM.Count+" sticks"
-        Disks           = $Disks
-	    Monitors	    = $Monitors
+    $computerdeets  = [pscustomobject]@{
+        MoBo        = $MoBo.Manufacturer+" "+$MoBo.Product
+        CPU         = $CPU.DeviceID+" | "+$CPU.Name
+        GPU         = $GPU.Name+" | "+[math]::round($GPU.AdapterRAM / 1GB)+"GB | Driver version: "+$GPU.DriverVersion+" (published date: "+$GPU.DriverDate+")"
+        RAM         = ([math]::round(($RAM.Sum / 1GB))).ToString()+"GB in "+$RAM.Count+" sticks"
+        Disks       = $Disks
+	    Monitors	= $Monitors
     }
     ""
     "HARDWARE:"

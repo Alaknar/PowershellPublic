53957831cb8e264601db90d0726edefc82055a9c
Alaknar <alaknar+github@gmail.com>
2023-01-21 18:20:09 +0100

    Added support for laptop screens

--- Get-PCSpecs.ps1
+++ Get-PCSpecs.ps1
@@ -1,14 +1,23 @@
 function Get-PCSpecs {
-    $MoBo       = Get-CimInstance -ClassName Win32_BaseBoard
-    $CPU        = Get-CimInstance -ClassName Win32_Processor
-    $GPU        = Get-CimInstance -ClassName Win32_VideoController
-    $RAM        = Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum
-    $Disks      = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Select-Object -Property DeviceID, VolumeName, @{Name = "TotalSize"; Expression = { ([math]::round(($_.Size / 1GB))).ToString() + " GB" } }, @{Name = "FreeSpaceGB"; Expression = { ([math]::round(($_.FreeSpace / 1GB))).ToString() + " GB" } }
-    $Monitors   = Get-CimInstance -Namespace root\wmi -ClassName wmimonitorid | ForEach-Object {
-        New-Object -TypeName psobject -Property @{
-            Manufacturer = ($_.ManufacturerName -notmatch '^0$' | ForEach-Object { [char]$_ }) -join ""
-            Name         = ($_.UserFriendlyName -notmatch '^0$' | ForEach-Object { [char]$_ }) -join ""
-            Serial       = ($_.SerialNumberID -notmatch '^0$' | ForEach-Object { [char]$_ }) -join ""
+    $MoBo = Get-CimInstance -ClassName Win32_BaseBoard
+    $CPU = Get-CimInstance -ClassName Win32_Processor
+    $GPU = Get-CimInstance -ClassName Win32_VideoController
+    $RAM = Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum
+    $Disks = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Select-Object -Property DeviceID, VolumeName, @{Name = "TotalSize"; Expression = { ([math]::round(($_.Size / 1GB))).ToString() + " GB" } }, @{Name = "FreeSpaceGB"; Expression = { ([math]::round(($_.FreeSpace / 1GB))).ToString() + " GB" } }
+    $Monitors = Get-CimInstance -Namespace root\wmi -ClassName wmimonitorid | ForEach-Object {
+        if ($_.UserFriendlyNameLength -eq 0) {
+            $monitor = Get-CimInstance -ClassName CIM_DesktopMonitor
+            New-Object -TypeName psobject -Property @{
+                Manufacturer = $monitor.MonitorManufacturer
+                Name         = $monitor.Name
+            }
+        }
+        else {
+            New-Object -TypeName psobject -Property @{
+                Manufacturer = ($_.ManufacturerName -notmatch '^0$' | ForEach-Object { [char]$_ }) -join ""
+                Name         = ($_.UserFriendlyName -notmatch '^0$' | ForEach-Object { [char]$_ }) -join ""
+                Serial       = ($_.SerialNumberID   -notmatch '^0$' | ForEach-Object { [char]$_ }) -join ""
+            }
         }
     }
 
